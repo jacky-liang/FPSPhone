553b46746d1c2178b67a1e9d19ebe53517e720dd
Jacky <jackyliang@berkeley.edu>
2015-01-30 22:23:52 -0800

    bluetooth branch

--- app/src/main/java/com/fpsphone/MainActivity.java
+++ app/src/main/java/com/fpsphone/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends Activity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+        
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
