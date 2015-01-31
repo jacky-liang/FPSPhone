db8f6dd11e918e2200457813098d04e7a793603d
Christopher Luong <bumblefo@gmail.com>
2015-01-31 02:32:10 -0800

    added gyroscope function

--- app/src/main/java/com/fpsphone/MainActivity.java
+++ app/src/main/java/com/fpsphone/MainActivity.java
@@ -1,15 +1,22 @@
 package com.fpsphone;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-//import android.view.KeyEvent;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import org.w3c.dom.Text;
+
+public abstract class MainActivity extends Activity implements SensorEventListener{
 
     private TextView debugStatus;
     private Button btn_w;
@@ -18,12 +25,29 @@ public class MainActivity extends Activity {
     private Button btn_d;
     private Button btn_s;
 
+    private SensorManager aSensorManager;
+    private Sensor gyroscope;
+    private TextView debugGyroX;
+    private TextView debugGyroY;
+    private TextView debugGyroZ;
+
+    private final float EPSILON = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         debugStatus = (TextView) findViewById(R.id.debugStatus);
+
+        aSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        gyroscope = aSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+
+        debugGyroX = (TextView) findViewById(R.id.debugGyroX);
+        debugGyroY = (TextView) findViewById(R.id.debugGyroY);
+        debugGyroZ = (TextView) findViewById(R.id.debugGyroZ);
+
 
         btn_w = (Button) findViewById(R.id.buttonW);
         btn_w.setOnClickListener(new View.OnClickListener() {
@@ -63,6 +87,34 @@ public class MainActivity extends Activity {
     }
 
 
+    public void onSensorChanged(SensorEvent event) {
+        // This timestep's delta rotation to be multiplied by the current rotation
+        // after computing it from the gyro sample data.
+
+
+        // Axis of the rotation sample, not normalized yet.
+        float axisX = event.values[0];
+        float axisY = event.values[1];
+        float axisZ = event.values[2];
+
+        // Calculate the angular speed of the sample
+        float omegaMagnitude = (float) Math.sqrt(axisX*axisX + axisY*axisY + axisZ*axisZ);
+
+        // Normalize the rotation vector if it's big enough to get the axis
+        if (omegaMagnitude > EPSILON) {
+            axisX /= omegaMagnitude;
+            axisY /= omegaMagnitude;
+            axisZ /= omegaMagnitude;
+        }
+
+        debugGyroX.setText(Float.toString(axisX));
+        debugGyroY.setText(Float.toString(axisY));
+        debugGyroZ.setText(Float.toString(axisZ));
+
+
+
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -84,6 +136,20 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                debugStatus.setText("Pressed Volume Up");
+                break;
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                debugStatus.setText("Pressed Volume Down");
+                break;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
 
 
 
